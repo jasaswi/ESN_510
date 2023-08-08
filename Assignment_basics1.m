ebe5bce43471bc3225090dc35960450310ea50e4
jasaswi <141217833+jasaswi@users.noreply.github.com>
2023-08-09 01:17:47 +0530

    Add files via upload

---
+++ Assignment_basics1.m
@@ -0,0 +1,19 @@
+
+a = 20;
+b = 30;
+c = 40;
+d = 50; 
+e = input ("Enter a number \n");
+f = input ("Enter a number \n");
+g = input ("Enter a number \n");
+h = input ("Enter a number \n");
+
+p = a+e;
+q = b-f;
+r = c*g;
+s = d/h;
+
+fprintf ("The addition is = %f \n",p);
+fprintf ("The substraction is = %f \n", q);
+fprintf ("The multiplication = %f \n", r);
+fprintf ("The division = %f \n", s);
