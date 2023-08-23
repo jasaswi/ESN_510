86a7da3b120dd44c389f964c86765d0f4b3045b8
jasaswi <141217833+jasaswi@users.noreply.github.com>
2023-08-23 22:58:01 +0530

    Add files via upload

---
+++ inverse_transpose.m
@@ -0,0 +1,19 @@
+a =  [7 4 8;
+      2 4 6;
+      9 1 7]
+disp(a)
+b = inv(a)
+c = b'
+p = b + c
+q = a - b
+r = c * b
+s = c / b
+
+x = [2 2 2;
+     3 3 3;
+     4 4 4]
+
+
+y = x'
+z = inv(x)
+m = inv(y)
